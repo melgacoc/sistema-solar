1cb699bf04078448c2bf5ea1d6c437d6219faf5c
Claudio Melgaco <claudio19a@hotmail.com>
2022-07-24 09:23:34 -0300

    req 3 e 4

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
-    return (<div data-testid="solar-system">a</div>);
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
+    );
   }
 }
 
